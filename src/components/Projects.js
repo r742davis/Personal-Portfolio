c686d18db5e84de29fb02aa899d17abc4b96e6ca
Richard <richard.trow.davis@gmail.com>
2019-04-11 18:20:19 -0700

    Added css grid layout to projects page and arranged 6 different squares

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,6 +8,14 @@ class Projects extends Component {
         <div className="title-container">
           <h1 data-aos="fade-up">Projects Page</h1>
         </div>
+        <div className="projects-container">
+          <div className="project-grid" id="project-grid-1"></div>
+          <div className="project-grid" id="project-grid-2"></div>
+          <div className="project-grid" id="project-grid-3"></div>
+          <div className="project-grid" id="project-grid-4"></div>
+          <div className="project-grid" id="project-grid-5"></div>
+          <div className="project-grid" id="project-grid-6"></div>
+        </div>
       </section>
       </>
     )
