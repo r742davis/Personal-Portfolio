354de807f96623f56e0ccf58f50751cd5072662b
Richard <richard.trow.davis@gmail.com>
2019-04-14 11:14:33 -0700

    Added more skills and changed profile picture sizing and margins

--- src/components/About.js
+++ src/components/About.js
@@ -14,25 +14,26 @@ class About extends Component {
         </div>
         <div className="about-container" data-aos="fade-up">
           <img src={profile} className="profile-picture" alt="profile of richard"/>
-          <div className="description-container">
+          <div className="description-container" data-aos="fade-up">
             <h1>Richard Davis</h1>
             <h4>Full-Stack Web Developer</h4>
             <p>I am passionate coder who relishes diving into both structures and the UI experience. I specialize in React, MongoDB, and Ruby on Rails. I am always looking to learn something new and challenge myself as a coder everyday. </p>
             <div className="cv-button-container">
-              <MDBBtn className="cv-button" data-aos="fade-up"><a href="https://drive.google.com/open?id=1XL9bvtIZfgzgDIp401hvK6Y-RZ6N70GK" target="_blank">Download CV</a></MDBBtn>
+              <MDBBtn className="cv-button"><a href="https://drive.google.com/open?id=1XL9bvtIZfgzgDIp401hvK6Y-RZ6N70GK" target="_blank">Download CV</a></MDBBtn>
             </div>
           </div>
-          <div className="skills-list-container">
+          <div className="skills-list-container" data-aos="fade-up">
             <h1>Top Developer Skills</h1>
             <hr />
             <ul>
-              <li><i className="fas fa-check-circle skill-icon"></i>HTML/CSS</li>
-              <li><i className="fas fa-check-circle skill-icon"></i>JavaScript</li>
-              <li><i className="fas fa-check-circle skill-icon"></i>Ruby</li>
-              <li><i className="fas fa-check-circle skill-icon"></i>Ruby on Rails</li>
-              <li><i className="fas fa-check-circle skill-icon"></i>React</li>
-              <li><i className="fas fa-check-circle skill-icon"></i>PostgreSQL</li>
-              <li><i className="fas fa-check-circle skill-icon"></i>MongoDB</li>
+              <li><i className="fas fa-check-circle skill-icon html"></i>HTML/CSS</li>
+              <li><i className="fas fa-check-circle skill-icon javascript"></i>JavaScript</li>
+              <li><i className="fas fa-check-circle skill-icon javascript"></i>React</li>
+              <li><i className="fas fa-check-circle skill-icon javascript"></i>Node.js</li>
+              <li><i className="fas fa-check-circle skill-icon ruby"></i>Ruby</li>
+              <li><i className="fas fa-check-circle skill-icon ruby"></i>Ruby on Rails</li>
+              <li><i className="fas fa-check-circle skill-icon databases"></i>PostgreSQL</li>
+              <li><i className="fas fa-check-circle skill-icon databases"></i>MongoDB</li>
             </ul>
             <hr />
           </div>
