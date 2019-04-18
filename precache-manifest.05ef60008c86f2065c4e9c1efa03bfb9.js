f2f9af4e01b0d2eb3045b4e88e4bb439405becd6
Richard <richard.trow.davis@gmail.com>
2019-04-17 18:12:36 -0700

    Updates

---
+++ precache-manifest.05ef60008c86f2065c4e9c1efa03bfb9.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/portfolio_1/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "2198a9f21664f5c62278",
+    "url": "/portfolio_1/static/css/main.911873ec.chunk.css"
+  },
+  {
+    "revision": "8260531fc1cbdc74fc80",
+    "url": "/portfolio_1/static/js/runtime~main.f549b8ce.js"
+  },
+  {
+    "revision": "dc4bb86857f3aa226a14426eccd7ad7c",
+    "url": "/portfolio_1/static/media/starry-night.dc4bb868.jpg"
+  },
+  {
+    "revision": "3fce271bd182aa26efcc",
+    "url": "/portfolio_1/static/js/2.92908d5e.chunk.js"
+  },
+  {
+    "revision": "9c9fd70be09a74176bcfca8565bfa54d",
+    "url": "/portfolio_1/static/media/richard-davis.9c9fd70b.jpeg"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/portfolio_1/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/portfolio_1/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/portfolio_1/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/portfolio_1/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/portfolio_1/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/portfolio_1/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/portfolio_1/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/portfolio_1/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/portfolio_1/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/portfolio_1/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/portfolio_1/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/portfolio_1/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/portfolio_1/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/portfolio_1/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/portfolio_1/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/portfolio_1/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/portfolio_1/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/portfolio_1/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/portfolio_1/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "2198a9f21664f5c62278",
+    "url": "/portfolio_1/static/js/main.1dfcc281.chunk.js"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/portfolio_1/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/portfolio_1/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/portfolio_1/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/portfolio_1/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/portfolio_1/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/portfolio_1/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/portfolio_1/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/portfolio_1/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/portfolio_1/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/portfolio_1/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/portfolio_1/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/portfolio_1/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/portfolio_1/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/portfolio_1/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/portfolio_1/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/portfolio_1/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/portfolio_1/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "fedf7c222c39cca1de068caca9d50cd4",
+    "url": "/portfolio_1/static/media/computer.fedf7c22.jpg"
+  },
+  {
+    "revision": "9e212a233f0f7221aa3eebdff458748d",
+    "url": "/portfolio_1/static/media/cactus.9e212a23.jpg"
+  },
+  {
+    "revision": "789a688222edc356062841ccd9485c13",
+    "url": "/portfolio_1/static/media/lily-flower.789a6882.jpg"
+  },
+  {
+    "revision": "b1f51b9f0a4e6854441ac22af393374f",
+    "url": "/portfolio_1/static/media/sky-river.b1f51b9f.jpg"
+  },
+  {
+    "revision": "70764b84984982244ae3c0f385e9b9fb",
+    "url": "/portfolio_1/static/media/comp-on-desk.70764b84.jpg"
+  },
+  {
+    "revision": "51b477cd3b7b7cc7f51ff79f32036b88",
+    "url": "/portfolio_1/static/media/retro-computers.51b477cd.jpg"
+  },
+  {
+    "revision": "3fce271bd182aa26efcc",
+    "url": "/portfolio_1/static/css/2.5b6f32a7.chunk.css"
+  },
+  {
+    "revision": "56302cf7d6a42f8f14f20b2231f61ea4",
+    "url": "/portfolio_1/index.html"
+  }
+];
