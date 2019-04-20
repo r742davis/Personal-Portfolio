8960dac3b8eb967e7009d0c22f6a9b384b3554e8
Richard <richard.trow.davis@gmail.com>
2019-04-20 10:47:20 -0700

    Replaced core skills section with a separate backend section and changed the tech skills to frontend skills

--- src/components/About.js
+++ src/components/About.js
@@ -39,33 +39,29 @@ class About extends Component {
             <div className="skills-container">
               <div className="tech-skills-container">
                 <div className="tech-skills-title">
-                  <h4>Tech Skills</h4>
+                  <h4>Frontend Skills</h4>
                 </div>
                 <ul>
                   <li><img src="https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png" className="skill-icon"/>HTML/CSS</li>
                   <li><img src="https://sdtimes.com/wp-content/uploads/2018/01/bootstrap-stack.png" className="skill-icon"/>Bootstrap</li>
                   <li><img src="https://cdn.worldvectorlogo.com/logos/sass-1.svg" className="skill-icon"/>Sass</li>
                   <li><img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" className="skill-icon"/>React</li>
+                  <li><img src="https://cdn.freebiesupply.com/logos/large/2x/redux-logo-png-transparent.png" className="skill-icon"/>Redux</li>
                   <li><img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/thumb/angular2.png" className="skill-icon"/>AngularJS</li>
-                  <li><img src="https://www.balena.io/docs//img/language/nodejs.svg" className="skill-icon"/>Node.js</li>
                   <li><img src="https://img.icons8.com/color/48/000000/ruby-gemstone.png" className="skill-icon"/>Ruby</li>
-                  <li><img src="https://s3-eu-west-1.amazonaws.com/devinterface-web/production/technologies/40/medium/rubylogo111.png?1444285834" className="skill-icon"/>Ruby on Rails</li>
-                  <li><img src="https://cdn-images-1.medium.com/max/449/1*NRCOyVBY6Jiqr4Q9A1zoaQ.png" className="skill-icon"/>MongoDB/Express</li>
-                  <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/540px-Postgresql_elephant.svg.png" className="skill-icon"/>PostgreSQL</li>
+                  <li><img src="http://pluspng.com/img-png/jquery-logo-png-kategoriedateilogo-software-wikipedia-256.png" className="skill-icon"/>jQuery</li>
                 </ul>
               </div>
               <div className="core-skills-container">
                 <div className="core-skills-title">
-                  <h4>Core Skills</h4>
+                  <h4>Backend Skills</h4>
                 </div>
                 <ul>
-                  <li>Communication</li>
-                  <li>Teamwork</li>
-                  <li>Teaching</li>
-                  <li>Empathy</li>
-                  <li>Receptive to New Idea</li>
-                  <li>Contributive</li>
-                  <li>Personable</li>
+                  <li><img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png" className="skill-icon"/>Express</li>
+                  <li><img src="https://cdn-images-1.medium.com/max/449/1*NRCOyVBY6Jiqr4Q9A1zoaQ.png" className="skill-icon"/>MongoDB</li>
+                  <li><img src="https://www.balena.io/docs//img/language/nodejs.svg" className="skill-icon"/>Node.js</li>
+                  <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/540px-Postgresql_elephant.svg.png" className="skill-icon"/>PostgreSQL</li>
+                  <li><img src="https://s3-eu-west-1.amazonaws.com/devinterface-web/production/technologies/40/medium/rubylogo111.png?1444285834" className="skill-icon"/>Ruby on Rails</li>
                 </ul>
               </div>
             </div>
