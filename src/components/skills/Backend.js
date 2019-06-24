37fcfece2fe228f7ea762cb5f24ced5076e60f57
Richard <richard.trow.davis@gmail.com>
2019-06-24 13:20:38 -0700

    Added all other backend skills to state, deleted img elements in JSX

--- src/components/skills/Backend.js
+++ src/components/skills/Backend.js
@@ -10,24 +10,24 @@ const backend = () => {
         alt: 'express'
       },
       {
-        name: 'Express.js',
-        link: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png',
-        alt: 'express'
+        name: 'MongoDB',
+        link: 'https://cdn-images-1.medium.com/max/449/1*NRCOyVBY6Jiqr4Q9A1zoaQ.png',
+        alt: 'mongoDB'
       },
       {
-        name: 'Express.js',
-        link: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png',
-        alt: 'express'
+        name: 'Node.js',
+        link: 'https://www.balena.io/docs//img/language/nodejs.svg',
+        alt: 'node js'
       },
       {
-        name: 'Express.js',
-        link: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png',
-        alt: 'express'
+        name: 'PostgreSQL',
+        link: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/540px-Postgresql_elephant.svg.png',
+        alt: 'postgreSQL'
       },
       {
-        name: 'Express.js',
-        link: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/thumb/expressjslogo.png',
-        alt: 'express'
+        name: 'Roby on Rails',
+        link: 'https://s3-eu-west-1.amazonaws.com/devinterface-web/production/technologies/40/medium/rubylogo111.png?1444285834g',
+        alt: 'ruby on rails'
       }
     ]
   });
@@ -47,24 +47,7 @@ const backend = () => {
           <h4>Backend Skills</h4>
         </div>
         <ul>
-        {skillList}
-
-          <li><img
-            src="https://cdn-images-1.medium.com/max/449/1*NRCOyVBY6Jiqr4Q9A1zoaQ.png"
-            className="skill-icon"
-            alt="mongo db"/>MongoDB</li>
-          <li><img
-            src="https://www.balena.io/docs//img/language/nodejs.svg"
-            className="skill-icon"
-            alt="node js"/>Node.js</li>
-          <li><img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/540px-Postgresql_elephant.svg.png"
-            className="skill-icon"
-            alt="Postgre SQL"/>PostgreSQL</li>
-          <li><img
-            src="https://s3-eu-west-1.amazonaws.com/devinterface-web/production/technologies/40/medium/rubylogo111.png?1444285834"
-            className="skill-icon"
-            alt="ruby on rails"/>Ruby on Rails</li>
+          {skillList}
         </ul>
       </div>
     </>
