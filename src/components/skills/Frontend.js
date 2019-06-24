9ed5a5d5c84d729b5b3b06b49f8827cbb446c81f
Richard <richard.trow.davis@gmail.com>
2019-06-24 13:47:21 -0700

    Removed all keyes in state and instead passed through index as key

--- src/components/skills/Frontend.js
+++ src/components/skills/Frontend.js
@@ -5,10 +5,39 @@ const frontend = () => {
   const [ skillState, setSkillState ] = useState({
     skills: [
       {
-        key: 1,
         name: 'AngularJS',
         link: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/300/thumb/angular2.png',
         alt: 'angular js'
+      },
+      {
+        name: 'Bootstrap 4',
+        link: 'https://sdtimes.com/wp-content/uploads/2018/01/bootstrap-stack.png',
+        alt: 'Bootstrap 4'
+      },
+      {
+        name: 'CSS3',
+        link: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl_F-GgXnRsNYBTfuA-e9l84ZIz46UjUkgw3jufMIMrdS2QaemQg',
+        alt: 'CSS 3'
+      },
+      {
+        name: 'HTML5',
+        link: 'https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png',
+        alt: 'HTML 5'
+      },
+      {
+        name: 'JavaScript',
+        link: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+        alt: 'JavaScript'
+      },
+      {
+        name: 'jQuery',
+        link: 'http://pluspng.com/img-png/jquery-logo-png-kategoriedateilogo-software-wikipedia-256.png',
+        alt: 'jQuery'
+      },
+      {
+        name: '',
+        link: '',
+        alt: ''
       },
       {
         name: '',
@@ -27,9 +56,9 @@ const frontend = () => {
       }
     ]
   });
-  const skillList = skillState.skills.map(skill => {
+  const skillList = skillState.skills.map((skill, index) => {
     return <li><img
-      key={skill.key}
+      key={index}
       src={skill.link}
       className='skill-icon'
       alt={skill.alt}
@@ -42,26 +71,6 @@ const frontend = () => {
       </div>
       <ul>
         {skillList}
-        <li><img
-          src="https://sdtimes.com/wp-content/uploads/2018/01/bootstrap-stack.png"
-          className="skill-icon"
-          alt="bootstrap"/>Bootstrap</li>
-        <li><img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl_F-GgXnRsNYBTfuA-e9l84ZIz46UjUkgw3jufMIMrdS2QaemQg"
-          className="skill-icon"
-          alt="css"/>CSS3</li>
-        <li><img
-          src="https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png"
-          className="skill-icon"
-          alt="html"/>HTML5</li>
-        <li><img
-          src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-          className="skill-icon"
-          alt="javascript"/>JavaScript</li>
-        <li><img
-          src="http://pluspng.com/img-png/jquery-logo-png-kategoriedateilogo-software-wikipedia-256.png"
-          className="skill-icon"
-          alt="j query"/>jQuery</li>
         <li><img
           src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"
           className="skill-icon"
