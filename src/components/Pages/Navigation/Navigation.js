08a649953518098b8eac9336c03a27f363ed47d7
Richard Davis <richard.trow.davis@gmail.com>
2020-03-31 15:35:57 -0400

    Added navbar burger and functionality that reveals links

--- src/components/Pages/Navigation/Navigation.js
+++ src/components/Pages/Navigation/Navigation.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import './Navigation.css';
+import React, { Component } from "react";
+import "./Navigation.css";
+import Burger from "@animated-burgers/burger-squeeze";
+import "@animated-burgers/burger-squeeze/dist/styles.css";
 import {
   Collapse,
   Navbar,
@@ -8,42 +10,66 @@ import {
   Nav,
   NavItem,
   NavLink
-} from 'reactstrap';
+} from "reactstrap";
 
-const navigation = (props) => {
-    const navClass = props.scrolled ? 'nav-link section' : 'nav-link-recolored section'
-    const navBar = props.scrolled ? "navbar" : "navbar-recolored";
-    const navBrand = props.scrolled ? "navbar-brand text-white" : "navbar-brand-recolored"
+class Navigation extends Component {
+  state = {
+    burgerOpen: false
+  };
 
+  toggleBurger = () => {
+    this.setState({
+      burgerOpen: !this.state.burgerOpen
+    });
+  };
+
+  render() {
+    const navClass = this.props.scrolled
+      ? "nav-link section"
+      : "nav-link-recolored section";
+    const navBar = this.props.scrolled ? "navbar" : "navbar-recolored";
+    const navBrand = this.props.scrolled
+      ? "navbar-brand text-white"
+      : "navbar-brand-recolored";
     return (
       <>
-        <Navbar
-          light
-          expand="md"
-          className={navBar}
-        >
-          <NavbarBrand
-            className={navBrand}>R</NavbarBrand>
-          <NavbarToggler onClick={props.toggle} />
-          <Collapse isOpen={props.isOpen} navbar>
+        <Navbar light expand="md" className={navBar}>
+          <NavbarBrand className={navBrand}>
+            <Burger
+              isOpen={this.state.burgerOpen}
+              onClick={() => this.toggleBurger()}
+            />
+          </NavbarBrand>
+          {this.state.burgerOpen &&
             <Nav className="ml-auto navbar-center" navbar>
-              <NavItem>
-                <NavLink href="#home" className={navClass}>Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#about" className={navClass}>About</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#projects" className={navClass}>Projects</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#contact" className={navClass}>Contact</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
+            <NavItem>
+              <NavLink 
+                href="#about" 
+                className={`${navClass} slide-in`}>
+                About
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink 
+                href="#projects" 
+                className={`${navClass} slide-in`}>
+                Projects
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink 
+                href="#contact" 
+                className={`${navClass} slide-in`}>
+                Contact
+              </NavLink>
+            </NavItem>
+          </Nav>
+          }
+          
         </Navbar>
       </>
     );
+  }
 }
 
-export default navigation;
+export default Navigation;
