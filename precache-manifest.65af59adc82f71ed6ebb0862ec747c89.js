a2e1f527e1945e0f722f14bbe5d48818127110b5
Richard Davis <richard.trow.davis@gmail.com>
2020-04-01 13:03:11 -0400

    Updates

---
+++ precache-manifest.65af59adc82f71ed6ebb0862ec747c89.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "www.richardtrowdavis.com/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "f9dd99574399042e4e06",
+    "url": "www.richardtrowdavis.com/static/css/main.dd293063.chunk.css"
+  },
+  {
+    "revision": "f525d6e8773a4077c180",
+    "url": "www.richardtrowdavis.com/static/js/runtime~main.88cee2be.js"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "www.richardtrowdavis.com/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "f5250b900f086d906622",
+    "url": "www.richardtrowdavis.com/static/js/2.39b9b58c.chunk.js"
+  },
+  {
+    "revision": "c00129be5a29d1bf10051657df17b23c",
+    "url": "www.richardtrowdavis.com/static/media/mongodb.c00129be.png"
+  },
+  {
+    "revision": "582bae12ee9ad893ff4caef9bf1bd08a",
+    "url": "www.richardtrowdavis.com/static/media/node.582bae12.png"
+  },
+  {
+    "revision": "ea5a6b4e047b0d965fbef3699466da8d",
+    "url": "www.richardtrowdavis.com/static/media/postgresql.ea5a6b4e.png"
+  },
+  {
+    "revision": "e8b602700ebdac7dd369834a3275bc46",
+    "url": "www.richardtrowdavis.com/static/media/graphql.e8b60270.png"
+  },
+  {
+    "revision": "8febe1a13f181d25b478f655c8011a01",
+    "url": "www.richardtrowdavis.com/static/media/ruby_on_rails.8febe1a1.png"
+  },
+  {
+    "revision": "7384412324c769e447d3c4f86e9ff068",
+    "url": "www.richardtrowdavis.com/static/media/bootstrap.73844123.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "www.richardtrowdavis.com/static/media/javascript.0cded3a3.png"
+  },
+  {
+    "revision": "db8e895b3935cf67e86ff99cc2e15cce",
+    "url": "www.richardtrowdavis.com/static/media/jquery.db8e895b.png"
+  },
+  {
+    "revision": "f9dd99574399042e4e06",
+    "url": "www.richardtrowdavis.com/static/js/main.03905296.chunk.js"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "www.richardtrowdavis.com/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "8e766e9562509cb3fa14264580c5c0f9",
+    "url": "www.richardtrowdavis.com/static/media/sass.8e766e95.svg"
+  },
+  {
+    "revision": "af121b3aa716a75ed5afa0e160a48903",
+    "url": "www.richardtrowdavis.com/static/media/Richard-cropped.af121b3a.jpeg"
+  },
+  {
+    "revision": "1f8a714077aadb73820a65cc7b487e4a",
+    "url": "www.richardtrowdavis.com/static/media/harness-wealth-test.1f8a7140.png"
+  },
+  {
+    "revision": "21fd6da0996dbae479aca80014cb0cb1",
+    "url": "www.richardtrowdavis.com/static/media/bear.21fd6da0.jpg"
+  },
+  {
+    "revision": "966d21bcc9e115ce2bf7ac8e4bd157ed",
+    "url": "www.richardtrowdavis.com/static/media/retro-computers.966d21bc.jpg"
+  },
+  {
+    "revision": "71b46dde9fddeec78d090183a8abb71d",
+    "url": "www.richardtrowdavis.com/static/media/wine.71b46dde.jpg"
+  },
+  {
+    "revision": "abbd60a45ff92d6ade62e4fd96ff5d30",
+    "url": "www.richardtrowdavis.com/static/media/traveleritis.abbd60a4.jpg"
+  },
+  {
+    "revision": "2edbc4152c25a2c023a39a10cabd9d4c",
+    "url": "www.richardtrowdavis.com/static/media/bakersdozen.2edbc415.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "www.richardtrowdavis.com/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "f5250b900f086d906622",
+    "url": "www.richardtrowdavis.com/static/css/2.9a446954.chunk.css"
+  },
+  {
+    "revision": "3732770dcf3dcac5553636d7ee9016bd",
+    "url": "www.richardtrowdavis.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2f08069ad890af8e751ef0a73f0a83c.js"
+  "www.richardtrowdavis.com/precache-manifest.65af59adc82f71ed6ebb0862ec747c89.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("www.richardtrowdavis.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
