0868f9f4c762b41b43849f48ced0db8ecd0db8ab
Richard Davis <richard.trow.davis@gmail.com>
2020-03-31 23:34:00 -0400

    Fixed most of the modal styling to be consistent with the rest of the portfolio. Still need to work on the fonts and add buttons for the modal

--- src/components/Pages/Projects/Modal/Modal.js
+++ src/components/Pages/Projects/Modal/Modal.js
@@ -8,29 +8,30 @@ const modal = props => {
         <div className="modal__grid">
           <div className="modal__close">
             <i onClick={props.closeModal} className="fas fa-times"></i>
+            <h1>{props.name}</h1>
           </div>
-          <div className="modal__project-picture">
-            <img src={props.image} alt={props.alt} />
-          </div>
-          <div className="modal__project-title">
-            <h2>{props.name}</h2>
-          </div>
-          <div className="modal__project-description">
-            <p>{props.description}</p>
-          </div>
-          <div className="modal__project-tech">
-            <h4>Tech:</h4>
-          </div>
-          <div className="modal__project-tech-description">
-            <p>{props.tech}</p>
-          </div>
-          <div className="modal__project-links">
-            <h4>Links:</h4>
-          </div>
-          <div className="modal__project-links-description">
-            <a href={props.link1}>{props.linkName1}</a>
-            <a href={props.link2}>{props.linkName2}</a>
-            <a href={props.link3}>{props.linkName3}</a>
+          <div className="modal__project-container">
+            <div className="modal__project-picture">
+              <img src={props.image} alt={props.alt} />
+            </div>
+            <div className="modal__project-title"></div>
+            <div className="modal__project-description">
+              <p>{props.description}</p>
+            </div>
+            <div className="modal__project-tech">
+              <h4>Tech:</h4>
+            </div>
+            <div className="modal__project-tech-description">
+              <p>{props.tech}</p>
+            </div>
+            <div className="modal__project-links">
+              <h4>Links:</h4>
+            </div>
+            <div className="modal__project-links-description">
+              <a href={props.link1}>{props.linkName1}</a>
+              <a href={props.link2}>{props.linkName2}</a>
+              <a href={props.link3}>{props.linkName3}</a>
+            </div>
           </div>
         </div>
       </div>
