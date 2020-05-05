a5bf65b85035aa594be2ab12191c3ccad7d37e7f
Richard Davis <richard.trow.davis@gmail.com>
2020-05-05 15:25:30 -0700

    Modified Nav Bar styles so that it is more like how I want it. Committing before making major changes to the styles

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -4,25 +4,33 @@ import "./Navigation.css";
 const renderNav = (props) => (
   <nav
     onClick={() => props.toggleNav()}
-    className="nav__container nav__animation box-shadow"
+    className="nav__container nav__animation"
   >
-    <a href="#about" className="nav__link">
-      About
-    </a>
-    <a href="#projects" className="nav__link">
-      Projects
-    </a>
-    <a
-      href={props.resumeLink}
-      className="nav__link"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Resume
-    </a>
-    <a href="#contact" className="nav__link">
-      Contact
-    </a>
+    <li>
+      <a href="#about" className="nav__link">
+        About
+      </a>
+    </li>
+    <li>
+      <a href="#projects" className="nav__link">
+        Projects
+      </a>
+    </li>
+    <li>
+      <a
+        href={props.resumeLink}
+        className="nav__link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Resume
+      </a>
+    </li>
+    <li>
+      <a href="#contact" className="nav__link">
+        Contact
+      </a>
+    </li>
   </nav>
 );
 
