cb917887d17b35e4ea1d28293c98e9aab82011bb
Richard Davis <richard.trow.davis@gmail.com>
2020-06-01 15:58:23 -0700

    Removed Animation scss code

--- src/components/Navigation/NavBar.js
+++ src/components/Navigation/NavBar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Navigation.css";
-import Animation from "./Animation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAddressCard,
